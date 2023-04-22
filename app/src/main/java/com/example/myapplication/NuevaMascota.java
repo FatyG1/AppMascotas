661f9d9bcb26ba715c7367cf73cbcd04b72fc0de
FatyG1 <fatima.garcia1988@gmail.com>
2023-04-22 22:35:10 +0200

    feat (NuevaMascota, dbMascotas) Creo metodos para insertar datos

--- app/src/main/java/com/example/myapplication/NuevaMascota.java
+++ app/src/main/java/com/example/myapplication/NuevaMascota.java
@@ -11,11 +11,12 @@ import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.example.myapplication.db.dbHelper;
+import com.example.myapplication.db.dbMascota;
 
 import java.util.UUID;
 
-public class NuevaMascota extends AppCompatActivity implements View.OnClickListener {
-    private EditText etNombre, etChip, etEdad, etRaza, etPeso;
+public class NuevaMascota extends AppCompatActivity  implements View.OnClickListener {
+    private EditText etNombre, etChip, etEdad, etRaza, etPeso, etSexo, etEsterilizado;
     private RadioButton rbSexM, rbSexH, rbEstS, rbEstN;
     private Button btGuardar, btBorrar;
 
@@ -29,6 +30,8 @@ public class NuevaMascota extends AppCompatActivity implements View.OnClickListe
         etEdad = findViewById(R.id.etEdad);
         etRaza = findViewById(R.id.etRaza);
         etPeso = findViewById(R.id.etPeso);
+        etEsterilizado = findViewById(R.id.etEsterilizado);
+        etSexo = findViewById(R.id.etSexo);
         rbSexM = findViewById(R.id.rbSexM);
         rbSexH = findViewById(R.id.rbSexH);
         rbEstS = findViewById(R.id.rbEstS);
@@ -44,14 +47,40 @@ public class NuevaMascota extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case (R.id.btGuardar):
+                //crea la bd
                 dbHelper DbHelper= new dbHelper(this);
                 SQLiteDatabase db = DbHelper.getWritableDatabase();
                 if(db != null){
-                    Toast.makeText(this, "bd creada", Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, "Mascota creada", Toast.LENGTH_LONG).show();
                 }else{
-                    Toast.makeText(this, "bd no creada", Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, "La mascota no se ha podido crear", Toast.LENGTH_LONG).show();
                 }
+
+                    // Inserta datos
+                dbMascota DbMascotas = new dbMascota(this);
+                long id= DbMascotas.insertarMascota(etNombre.getText().toString(), etChip.getText().toString(), etEdad.getText().toString(),
+                        etRaza.getText().toString(), etPeso.getText().toString(), etSexo.getText().toString(), etEsterilizado.getText().toString());
+
+                if(id>0){
+                    Toast.makeText(this, "Mascota guardada", Toast.LENGTH_LONG).show();
+                    limpiar();
+                }else{
+                    Toast.makeText(this, "Error al guardar la mascota", Toast.LENGTH_LONG).show();
+                }
+                break;
+
+
+            }
         }
+
+    private void limpiar(){
+        etChip.setText("");
+        etNombre.setText("");
+        etEdad.setText("");
+        etRaza.setText("");
+        etSexo.setText("");
+        etEsterilizado.setText("");
+        etPeso.setText("");
     }
 
 
